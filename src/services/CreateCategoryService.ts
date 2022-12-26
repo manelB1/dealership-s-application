3fe39f854a3b5180d1c4d7184110a1d21629dc50
Emanuel Barbosa <emanuellbarbosa@hotmail.com>
2022-12-25 22:56:46 -0300

    Feature: Created Services Com Principios de Solid

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,25 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+
+interface Irequest {
+    name: string;
+    description: string;
+}
+
+class CreateCateoryService {
+
+    constructor(private categoriesRepository: CategoriesRepository) {
+
+    }
+    execute({ name, description }: Irequest): void {
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error("category already exists")
+        };
+        this.categoriesRepository.create({ name, description })
+    }
+
+}
+
+export { CreateCateoryService };
