1a486fcab00366efd62fda269199684c58e19851
Emanuel Barbosa <emanuellbarbosa@hotmail.com>
2022-12-28 20:41:48 -0300

    Refactor: change message of log

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.json());
 app.use("/categories", categoriesRoutes);
 
-app.listen(3333, () => console.log("server is running"));
+app.listen(3333, () => console.log("server is running on port 3333"));
 
